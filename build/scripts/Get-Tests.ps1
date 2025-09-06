7abd716af11f9aa8e28b02ae802197f0d92e280b
Richard Bolhofer <20459042+SignalRichard@users.noreply.github.com>
2025-09-06 19:45:00 -0400

    build: fix get test cases for gha

--- build/scripts/Get-Tests.ps1
+++ build/scripts/Get-Tests.ps1
@@ -1,6 +1,8 @@
 $output = p check --list-tests
-$tests = @{ testcases = $output | Where-Object { $_.StartsWith("tc") } | Select-Object -Unique }
+#$tests = @{ testcases = $output | Where-Object { $_.StartsWith("tc") } | Select-Object -Unique }
+$tests = $output | Where-Object { $_.StartsWith("tc") } | Select-Object -Unique
 
 if($env:CI) {
-    Write-Output -InputObject (ConvertTo-Json -InputObject $tests -Depth 100 -Compress) >> $env:GITHUB_OUTPUT
+    #Write-Output -InputObject (ConvertTo-Json -InputObject $tests -Depth 100 -Compress) >> $env:GITHUB_OUTPUT
+    Write-Output -InputObject "testcases=$(ConvertTo-Json -InputObject $tests -Depth 100 -Compress)"
 }
